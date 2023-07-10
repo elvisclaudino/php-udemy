ef7d0dfc2aa53fffb7a7649ea2b5ae0abd41e6a8
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-07-10 13:24:51 -0300

    [add]: include and require once

---
+++ 8_inclusao_de_codigo/3_include_onde_require_once/index.php
@@ -0,0 +1,25 @@
+<?php
+
+  // Arquivos que não existem
+  // include_once "teste.php";
+
+  // Arquivos que existem
+  // include "teste2.php";
+  // include "teste2.php";
+
+  include_once "teste2.php";
+  include_once "teste2.php";
+
+  //Arquivos que não existem
+  // require_once "teste.php";
+
+  // Arquivos que existem
+  // include "teste3.php";
+  // include "teste3.php";
+
+  include_once "teste3.php";
+  include_once "teste3.php";
+
+?>
+
+<p>Testando código</p>
---
+++ 8_inclusao_de_codigo/3_include_onde_require_once/teste3.php
@@ -0,0 +1,5 @@
+<?php
+
+  echo "TESTANDO REQUIRE ONCE <br>";
+
+?>
