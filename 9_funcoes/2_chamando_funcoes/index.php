cc01f29d64135d658b9c349e0e3daeb91ae9616d
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-07-27 22:01:30 -0300

    [add]: call functions on php

---
+++ 9_funcoes/2_chamando_funcoes/index.php
@@ -0,0 +1,21 @@
+<?php
+
+  // Definindo função
+  function soma() {
+
+    $a = 5;
+    $b = 4;
+
+    // echo 4 + 2 . "<br>";
+    echo $a + $b . "<br>";
+    
+  }
+
+  // Chamando/invocando função
+  soma();
+  soma();
+
+  // Função do PHP
+  echo strtoupper("elvis");
+
+?>
