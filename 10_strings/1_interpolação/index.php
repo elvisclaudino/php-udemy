30382426be7f219f5a567963ddae2ecb5b4f7117
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-02 13:14:56 -0300

    [add]: Strings

---
+++ 10_strings/1_interpolação/index.php
@@ -0,0 +1,13 @@
+<?php
+
+  // O ideal é se manter apenas com uma das sintaxes
+
+  $nome = "Elvis";
+  $idade = 29;
+  $profissao = "Programador";
+
+  echo "Eu sou o $nome e tenho $idade anos, e atuo como $profissao <br>";
+
+  echo "Eu sou o {$nome} e tenho {$idade} anos, e atuo como {$profissao} <br>";
+
+?>
