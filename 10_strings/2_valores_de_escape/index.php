073f6b5b48e5f182dd552fbcb6b56a293e30c7e3
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-02 13:21:35 -0300

    [add]: escape strings

---
+++ 10_strings/2_valores_de_escape/index.php
@@ -0,0 +1,16 @@
+<?php
+
+  header("Content-type: text/plain");
+
+  // Pular linha
+  echo "Isso aqui vai ficar na primeira linha \n E isso na segunda linha \n";
+
+  // Tab
+  echo "Testando o tab \t aqui \n";
+
+  // Barra invertida
+  echo "Barra invertida \\ \n";
+
+  // Dólar
+  echo "Imprimindo o dólar \$teste";
+?>
