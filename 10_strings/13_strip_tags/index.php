73b803b6d5029bf496b233d0c89a09d885a6c211
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-03 12:56:53 -0300

    [add]: strip tags

---
+++ 10_strings/13_strip_tags/index.php
@@ -0,0 +1,11 @@
+<?php
+
+  $textoHtml = "<p>Testando parágrafo.</p><div>Uma div</div><p>Outro parágrafo</p>";
+
+  echo $textoHtml;
+  
+  $salvarTextoBanco = strip_tags($textoHtml);
+
+  echo $salvarTextoBanco
+
+?>
