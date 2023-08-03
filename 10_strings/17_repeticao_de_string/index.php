99a88051f4ff139793ab5a34daf0da3edb50ec73
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-03 13:22:15 -0300

    [add]: str repeat

---
+++ 10_strings/17_repeticao_de_string/index.php
@@ -0,0 +1,13 @@
+<?php
+
+  $str = "Teste";
+
+  $strRepetida = str_repeat($str, 5);
+
+  echo $strRepetida . "<br>";
+
+  $frase = "Testando repetição por frase ";
+
+  echo str_repeat($frase, 3)
+
+?>
