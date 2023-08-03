bb98d6eda4efdf0684e50e3f12488bfed954458e
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-03 00:37:39 -0300

    [add] exercise 41

---
+++ 10_strings/12_exercicio_41/index.php
@@ -0,0 +1,11 @@
+<?php
+
+  $frase1 = "este item está em";
+  $frase2 = "promoção";
+
+  $frase1Maiuscula = ucfirst($frase1);
+  $frase2Caps = strtoupper($frase2);
+
+  echo "$frase1Maiuscula $frase2Caps <br>";
+
+?>
