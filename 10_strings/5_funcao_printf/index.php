5844bbdf5b8a815a6fbee974677ae00ed246860e
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-03 00:06:23 -0300

    [add]: printf function

---
+++ 10_strings/5_funcao_printf/index.php
@@ -0,0 +1,19 @@
+<?php
+
+  $nome = "Elvis";
+
+  // %s -> string
+
+  printf("O nome é %s <br>", $nome);
+
+  $n = 10;
+
+  // %d -> int
+
+  printf("O número é %d e o outro é %d <br>", $n, 150);
+  
+  // %f -> float
+
+  printf("A temperatura atual é %.1f <br>", 12.58)
+
+?>
