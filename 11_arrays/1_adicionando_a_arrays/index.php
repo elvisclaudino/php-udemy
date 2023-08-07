9ff53001507c4719700e25aa7573c67dfac03252
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-07 15:43:04 -0300

    [add]: add items on arrays

---
+++ 11_arrays/1_adicionando_a_arrays/index.php
@@ -0,0 +1,51 @@
+<?php
+
+  $arr = [];
+
+  print_r($arr);
+  echo "<br>";
+
+  // Adicionando valores
+  $arr[0] = 10;
+
+  print_r($arr);
+  echo "<br>";
+
+  $arr[1] = 15;
+
+  print_r($arr);
+  echo "<br>";
+
+  $arr[5] = 15;
+
+  print_r($arr);
+  echo "<br>";
+
+  // Modificando valores
+
+  $arr[1] += 55;
+
+  print_r($arr);
+  echo "<br>";
+
+  $arrAssoc = [];
+
+  print_r($arrAssoc);
+  echo "<br>";
+
+  $arrAssoc["carro"] = "BMW";
+
+  print_r($arrAssoc);
+  echo "<br>";
+
+  $arrAssoc["avião"] = "Boeing";
+
+  print_r($arrAssoc);
+  echo "<br>";
+
+  $arrAssoc["carro"] = "Ferrari";
+
+  print_r($arrAssoc);
+  echo "<br>";
+
+?>
