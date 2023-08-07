1d1cfc893dc8e48386778b28012af691368141d4
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-07 15:46:24 -0300

    [add]: add item on end of array

---
+++ 11_arrays/2_adicionando_ao_fim_do_array/index.php
@@ -0,0 +1,32 @@
+<?php
+
+  $arr = [1,2,3];
+
+  print_r($arr);
+  echo "<br>";
+
+  $arr[] = 4;
+
+  print_r($arr);
+  echo "<br>";
+
+  $arr[] = 5;
+
+  print_r($arr);
+  echo "<br>";
+
+  $arr2 = [];
+
+  $arr2[] = 1;
+
+  print_r($arr2);
+  echo "<br>";
+
+  $arr3 = [];
+
+  $arr3['teste'] = 'testando';
+
+  print_r($arr3);
+  echo "<br>";
+
+?>
