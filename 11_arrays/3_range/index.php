bedceceda9ad06b2323ad61e56685080b2696396
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-07 15:49:09 -0300

    [add]: range

---
+++ 11_arrays/3_range/index.php
@@ -0,0 +1,22 @@
+<?php
+
+  // range(inicio, fim)
+
+  $arr = range(1, 10);
+
+  print_r($arr);
+  echo "<br>";
+
+  $arr2 = range(5, 50);
+
+  print_r($arr2);
+  echo "<br>";
+
+  // range(inicio, fim, passo)
+
+  $arr3 = range(0, 1000, 100);
+
+  print_r($arr3);
+  echo "<br>";
+
+?>
