374bd5223dcd2b0bdf2f82efb4d4aad94aa53dae
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-08 22:29:03 -0300

    [add]: list

---
+++ 11_arrays/8_funcao_list/index.php
@@ -0,0 +1,15 @@
+<?php
+
+  $pessoa = ["Matheus", 29, "Programador", "Verde"];
+
+  print_r($pessoa);
+  echo "<br>";
+
+  list($nome, $idade, $profissao, $corDosOlhos) = $pessoa;
+
+  echo "$nome <br>";
+  echo "$idade <br>";
+  echo "$profissao <br>";
+  echo "$corDosOlhos <br>";
+
+?>
