83c89f15d9e106aff63df9c1bbf4e22da815f48e
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-08 22:01:09 -0300

    [add]: count

---
+++ 11_arrays/5_numero_de_elementos/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$arr = [1,2,3];
+
+echo count($arr) . "<br>";
+
+$arr2 = range(1,10);
+
+echo count($arr2) . "<br>";
+
+$arr3 = ["nome" => "Elvis", "idade" => 19, "profissão" => "Programador"];
+
+echo count($arr3) . "<br>";
+
+?>
