69b7bce7cef5501d3b67a6327d16b396e1249019
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-10 14:15:38 -0300

    [add]: compact

---
+++ 11_arrays/17_compact/index.php
@@ -0,0 +1,13 @@
+<?php
+
+  $marca = "BMW";
+  $motor = "3.0";
+  $tetoSolar = true;
+  $portas = 4;
+
+  $carro = compact("marca", "motor", "tetoSolar", "portas");
+
+  print_r($carro);
+  echo "<br>";
+
+?>
