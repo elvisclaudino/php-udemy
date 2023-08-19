5c4f55f1e10ee8e265afff113a3963d83a03ab9b
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-18 23:26:17 -0300

    [add]: array reverse

---
+++ 11_arrays/26_array_reverse/index.php
@@ -0,0 +1,23 @@
+<?php
+
+  $arr = [1,2,3,4,5];
+
+  $arrRev = array_reverse($arr);
+
+  print_r($arr);
+  echo "<br>";
+
+  print_r($arrRev);
+  echo "<br>";
+
+  $arr2 = ['Elvis', 19, 1.20, true, [1,2,3]];
+
+  $arrRev2 = array_reverse($arr2);
+
+  print_r($arr2);
+  echo "<br>";
+
+  print_r($arrRev2);
+  echo "<br>";
+
+?>
